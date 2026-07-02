09617791a28679c436590f8c3763af2ba9de2f6c
kendrickt14 <kt99514@gmail.com>
2026-07-02 20:53:46 +0800

    Delete sw.js.js

--- sw.js.js
+++
@@ -1,8 +0,0 @@
-// Minimal Service Worker to satisfy Android Chrome PWA requirements
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', (e) => {
-    // Leave empty. This tells Android the app has networking capability.
-});
