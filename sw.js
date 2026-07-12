e4d9d76bc381fef90a0a78d9fbff9e8940e4adbf
Kendrick T <298591708+kendrickt14@users.noreply.github.com>
2026-07-12 15:51:56 +0800

    Update service worker cache version to 1.0.40

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Version: 1.0.37
-const CACHE_NAME = 'inhome-cache-v1.0.30';
+// Version: 1.0.40
+const CACHE_NAME = 'inhome-cache-v1.0.40';
 
 self.addEventListener('install', (e) => {
     self.skipWaiting(); // Force the waiting service worker to become the active service worker.
